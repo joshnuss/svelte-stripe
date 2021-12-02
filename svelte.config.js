63664eb31f6d739a835a116303b9c72425e19d3b
Josh Nussbaum <joshnuss@gmail.com>
2021-12-02 18:28:33 -0500

    Adds mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
+import { mdsvex } from 'mdsvex'
+import mdsvexConfig from './mdsvex.config.js'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
-  }
+  },
+
+  preprocess: [mdsvex(mdsvexConfig)]
 }
 
 export default config
