14d0d26883a87229641de99bb9bf8e66e353611d
Josh Nussbaum <joshnuss@gmail.com>
2021-12-03 01:21:55 -0500

    Adds mkcert plugin

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
+import mkcert from 'vite-plugin-mkcert'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
+    target: '#svelte',
+
+    vite: {
+      server: {
+        https: true,
+      },
+      plugins: [ mkcert.default({hosts: ['localhost', 'app.local']}) ]
+    }
   },
 
   preprocess: [mdsvex(mdsvexConfig)]
