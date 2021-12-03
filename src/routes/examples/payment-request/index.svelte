9c867758e81fdea7f2670234c9fce752b1d9b2dd
Josh Nussbaum <joshnuss@gmail.com>
2021-12-03 01:20:27 -0500

    Adds example of payment request button

---
+++ src/routes/examples/payment-request/index.svelte
@@ -0,0 +1,77 @@
+<script context="module">
+  export async function load({ fetch }) {
+    // create payment intent
+    const response = await fetch('/examples/payment-request/payment-intent', { method: 'POST' })
+    const { clientSecret } = await response.json()
+
+    // share payment intent's client secret
+    return {
+      props: {
+        clientSecret
+      }
+    }
+  }
+</script>
+
+<script>
+  import { goto } from '$app/navigation'
+  import { onMount } from 'svelte'
+  import { loadStripe } from '@stripe/stripe-js'
+  import { Container, PaymentRequestButton } from '$lib'
+
+  export let clientSecret
+
+  let stripe = null
+  let error = null
+
+  // define payment details
+  const paymentRequest = {
+    country: 'US',
+    currency: 'usd',
+    total: {label: 'Demo total', amount: 1099},
+    requestPayerName: true,
+    requestPayerEmail: true,
+  }
+
+  onMount(async () => {
+    stripe = await loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY)
+  })
+
+  async function pay(e) {
+    const paymentMethod = e.detail.paymentMethod
+
+    let result = await stripe.confirmCardPayment(clientSecret,
+      { payment_method: paymentMethod.id }
+    )
+
+    if (result.error) {
+      e.detail.complete('fail')
+
+      // payment failed, notify user
+      error = result.error
+    } else {
+      e.detail.complete('success')
+
+      // payment succeeded, redirect to "thank you" page
+      goto('/examples/payment-request/thanks')
+    }
+  }
+</script>
+
+<h1>Payment Request Example</h1>
+
+{#if error}
+  <p class=error>{error.message} Please try again.</p>
+{/if}
+
+{#if stripe}
+  <Container {stripe}>
+    <PaymentRequestButton {paymentRequest} on:paymentmethod={pay}/>
+  </Container>
+{:else}
+  Loading...
+{/if}
+
+<style>
+  .error { color: tomato }
+</style>
---
+++ src/routes/examples/payment-request/thanks.svelte
@@ -0,0 +1,2 @@
+<h1>Success!</h1>
+<p>Payment request was successfully processed.</p>
