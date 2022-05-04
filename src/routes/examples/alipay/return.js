8cfbb4b22b81efad689fe415674b9a8086310820
Josh Nussbaum <joshnuss@gmail.com>
2022-05-03 22:58:00 -0400

    Refactor search params

--- src/routes/examples/alipay/return.js
+++ src/routes/examples/alipay/return.js
@@ -3,8 +3,9 @@ import Stripe from 'stripe'
 const stripe = new Stripe(process.env['STRIPE_SECRET_KEY'])
 
 export async function get(req) {
-  const intentId = req.url.searchParams.get('payment_intent')
-  const clientSecret = req.url.searchParams.get('payment_intent_client_secret')
+  const { searchParams } = req.url
+  const intentId = searchParams.get('payment_intent')
+  const clientSecret = searchParams.get('payment_intent_client_secret')
   const paymentIntent = await stripe.paymentIntents.retrieve(intentId)
 
   if (paymentIntent.client_secret !== clientSecret)
