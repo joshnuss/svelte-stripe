a2dc90d2df16a543ce622e7f420da8b17779b056
Josh Nussbaum <joshnuss@gmail.com>
2023-01-11 22:56:20 -0500

    Fix webhooks for credit-card example

--- src/routes/examples/credit-card/webhooks/+server.js
+++ src/routes/examples/credit-card/webhooks/+server.js
@@ -3,14 +3,14 @@ import { SECRET_STRIPE_KEY, STRIPE_WEBHOOK_SECRET } from '$env/static/private'
 
 const stripe = new Stripe(SECRET_STRIPE_KEY)
 
-export async function POST(request) {
-  const rawBody = Buffer.from(request.rawBody)
-  const signature = request.headers['stripe-signature']
+export async function POST({ request }) {
+  const body = await request.text()
+  const signature = request.headers.get('stripe-signature')
 
   let event
 
   try {
-    event = stripe.webhooks.constructEvent(rawBody, signature, STRIPE_WEBHOOK_SECRET)
+    event = stripe.webhooks.constructEvent(body, signature, STRIPE_WEBHOOK_SECRET)
   } catch (err) {
     console.warn('⚠️  Webhook signature verification failed.', err.message)
 
