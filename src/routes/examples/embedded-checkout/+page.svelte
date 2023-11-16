f597644bc2dbb7977e3d89dc21f36809619b5ef8
Josh Nussbaum <joshnuss@gmail.com>
2023-11-16 18:25:33 -0500

    fix: Update embedded checkout example to use +page.server.js

--- src/routes/examples/embedded-checkout/+page.svelte
+++ src/routes/examples/embedded-checkout/+page.svelte
@@ -1,30 +1,16 @@
 <script>
-  import { goto } from '$app/navigation'
   import { onMount } from 'svelte'
   import { loadStripe } from '@stripe/stripe-js'
   import { PUBLIC_STRIPE_KEY } from '$env/static/public'
   import { EmbeddedCheckout } from '$lib'
 
+  export let data
+
   let stripe = null
-  let clientSecret = null
 
   onMount(async () => {
     stripe = await loadStripe(PUBLIC_STRIPE_KEY)
-    clientSecret = await createCheckoutSession()
   })
-
-  async function createCheckoutSession() {
-    const response = await fetch('/examples/embedded-checkout/checkout-session', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({})
-    })
-    const { clientSecret } = await response.json()
-
-    return clientSecret
-  }
 </script>
 
 <h1>Embedded Checkout Example</h1>
@@ -33,4 +19,4 @@
   <a href="https://github.com/joshnuss/svelte-stripe/tree/main/src/routes/examples/embedded-checkout">View code</a>
 </nav>
 
-<EmbeddedCheckout {stripe} {clientSecret}/>
+<EmbeddedCheckout {stripe} clientSecret={data.clientSecret}/>
