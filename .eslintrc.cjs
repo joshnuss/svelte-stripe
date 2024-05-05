70359d7dafef36505d397e4c3dfb579bd6361c2c
David Roizenman <david@hmnd.io>
2024-05-04 19:14:18 -0700

    chore: update dev dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,13 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'prettier', 'plugin:jsdoc/recommended'],
-  plugins: ['svelte3', 'jsdoc'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:jsdoc/recommended',
+    'plugin:svelte/recommended'
+  ],
+  plugins: ['jsdoc'],
+  overrides: [{ files: ['*.svelte'], parser: 'svelte-eslint-parser' }],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2019
