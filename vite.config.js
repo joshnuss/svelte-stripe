c23a5c3893f71afa701778e659a359e82e344215
Josh Nussbaum <joshnuss@gmail.com>
2024-09-22 17:04:33 -0400

    Fix vite error

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    proxy: {}
+  },
   plugins: [sveltekit(), basicSsl()]
 })
