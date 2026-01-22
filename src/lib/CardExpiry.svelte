7204e21dd3149b120142b7cc03dcf7cae6e3944c
Josh Nussbaum <joshnuss@gmail.com>
2026-01-21 19:57:09 -0500

    Removes unused locals

--- src/lib/CardExpiry.svelte
+++ src/lib/CardExpiry.svelte
@@ -2,8 +2,7 @@
   import type {
     StripeCardExpiryElementOptions as Options,
     StripeCardExpiryElement as Element,
-    StripeCardExpiryElementChangeEvent as ChangeEvent,
-    StripeError
+    StripeCardExpiryElementChangeEvent as ChangeEvent
   } from '@stripe/stripe-js'
   import type { ElementsContext } from './d.ts'
   import { getContext, onMount } from 'svelte'
